5e3ebf3395b5ac660142e5cbad0507da2ce7ec12
sasha-go <sasha.lfg@gmail.com>
2020-04-18 12:33:35 -0700

    added movie filter functions

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use(morgan(morganSetting))
 app.use(helmet())
 app.use(cors())
 
+// Validate Token
 app.use(function validateBearerToken(req, res, next) {
 	const apiToken = process.env.API_TOKEN;
 	const authToken = req.get('Authorization')
@@ -22,38 +23,47 @@ app.use(function validateBearerToken(req, res, next) {
 
 	next()
 })
-	
 
+// SERVER ERROR HANDLING
+app.use((error, req, res, next) => {
+	let response
+	if (process.env.NODE_ENV === 'production') {
+		response = { error: { message: 'server error' }}
+	} else {
+		response = { error }
+	}
+	res.status(500).json(response)
+})
+
+// MOVIE ROUTE AND FILTERS
 app.get('/movie', function handleGetMovie(req, res) {
 	let response = MOVIES;
 
+	// GENRE FILTER
 	if (req.query.genre) {
 		response = response.filter(movie =>
 			movie.genre.toLowerCase().includes(req.query.genre.toLowerCase()
 		))
 	}
 
-	// if(req.query.country) {
+	// COUNTRY FILTER
+	if(req.query.country) {
+		response = response.filter(movie => {
+			movie.country.toLowerCase().includes(req.query.country.toLowerCase());
+		})
+	}
 
-	// }
-
-	// if(req.query.avg_vote) {
-
-	// }
+	// AVG VOTE FILTER
+	if(req.query.avg_vote) {
+		response = response.filter(movie => {
+			Number(movie.avg_vote >= Number(req.query.avg_vote))
+		})
+	}
 
 	res.json(response);
 
 })
 
-app.use((error, req, res, next) => {
-  let response
-  if (process.env.NODE_ENV === 'production') {
-    response = { error: { message: 'server error' }}
-  } else {
-    response = { error }
-  }
-  res.status(500).json(response)
-})
 
 const PORT = process.env.PORT || 8000;
 
